f834835c1f0794bbeb56712b178253d884995eb6
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-07 07:57:48 +0100

    Rewrite day 7 in a saner way

--- src/day7.rs
+++ src/day7.rs
@@ -24,151 +24,59 @@ enum Kind {
     Five,
 }
 
-fn kind(mut hand: [u8; 5]) -> Kind {
-    hand.sort();
+fn kind<const PART2: bool>(cards: [u8; 5]) -> Kind {
+    let count = |card| cards.iter().filter(|&&c| c == card).count();
 
-    let mut counts = HashMap::<u8, u32>::new();
-    for b in hand {
-        *counts.entry(b).or_default() += 1;
-    }
+    let mut counts = cards.map(count);
+    counts.sort_unstable();
 
-    let mut lcounts = counts.values().copied().collect::<Vec<_>>();
-    lcounts.sort();
+    let countj = if PART2 { count(b'J') } else { 0 };
 
-    if lcounts == [5] {
-        return Kind::Five;
+    match (counts, countj) {
+        ([5, 5, 5, 5, 5], _) => Kind::Five,
+        ([1, 4, 4, 4, 4], 0) => Kind::Four,
+        ([1, 4, 4, 4, 4], _) => Kind::Five,
+        ([2, 2, 3, 3, 3], 0) => Kind::Full,
+        ([2, 2, 3, 3, 3], _) => Kind::Five,
+        ([1, 1, 3, 3, 3], 0) => Kind::Three,
+        ([1, 1, 3, 3, 3], _) => Kind::Four,
+        ([1, 2, 2, 2, 2], 0) => Kind::Two,
+        ([1, 2, 2, 2, 2], 1) => Kind::Full,
+        ([1, 2, 2, 2, 2], 2) => Kind::Four,
+        ([1, 1, 1, 2, 2], 0) => Kind::One,
+        ([1, 1, 1, 2, 2], _) => Kind::Three,
+        ([1, 1, 1, 1, 1], 0) => Kind::High,
+        ([1, 1, 1, 1, 1], _) => Kind::One,
+        _ => unreachable!(),
     }
-    if lcounts == [1, 4] {
-        return Kind::Four;
-    }
-    if lcounts == [2, 3] {
-        return Kind::Full;
-    }
-    if lcounts == [1, 1, 3] {
-        return Kind::Three;
-    }
-    if lcounts == [1, 2, 2] {
-        return Kind::Two;
-    }
-    if lcounts == [1, 1, 1, 2] {
-        return Kind::One;
-    }
-    if lcounts == [1, 1, 1, 1, 1] {
-        return Kind::High;
-    }
-
-    panic!()
 }
 
-fn n(card: u8) -> u8 {
+fn n<const PART2: bool>(card: u8) -> u8 {
     match card {
         b'A' => 14,
         b'K' => 13,
         b'Q' => 12,
+        b'J' if PART2 => 0,
         b'J' => 11,
         b'T' => 10,
         _ => card - b'0',
     }
 }
 
-pub fn part1(input: &Input) -> usize {
+fn solve<const PART2: bool>(input: &Input) -> usize {
     input
         .iter()
-        .map(|&(cards, bid)| (kind(cards), cards.map(n), cards, bid))
+        .map(|&(cards, bid)| (kind::<PART2>(cards), cards.map(n::<PART2>), bid))
         .sorted()
         .enumerate()
-        .map(|(i, (_, _, _, bid))| (i + 1) * bid)
+        .map(|(i, (_, _, bid))| (i + 1) * bid)
         .sum()
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-enum Kind2 {
-    High,
-    One,
-    Two,
-    Three,
-    Full,
-    Four,
-    Five,
-}
-
-fn kind2(mut hand: [u8; 5]) -> Kind2 {
-    hand.sort();
-
-    let mut counts = HashMap::<u8, u32>::new();
-    for b in hand {
-        *counts.entry(b).or_default() += 1;
-    }
-
-    let countj = *counts.get(&b'J').unwrap_or(&0);
-
-    let mut lcounts = counts.values().copied().collect::<Vec<_>>();
-    lcounts.sort();
-
-    if lcounts == [5] {
-        return Kind2::Five;
-    }
-    if lcounts == [1, 4] && countj != 0 {
-        return Kind2::Five;
-    }
-    if lcounts == [1, 4] {
-        return Kind2::Four;
-    }
-    if lcounts == [2, 3] && countj != 0 {
-        return Kind2::Five;
-    }
-    if lcounts == [2, 3] {
-        return Kind2::Full;
-    }
-    if lcounts == [1, 1, 3] && countj != 0 {
-        return Kind2::Four;
-    }
-    if lcounts == [1, 1, 3] {
-        return Kind2::Three;
-    }
-    if lcounts == [1, 2, 2] && countj == 2 {
-        return Kind2::Four;
-    }
-    if lcounts == [1, 2, 2] && countj == 1 {
-        return Kind2::Full;
-    }
-    if lcounts == [1, 2, 2] {
-        return Kind2::Two;
-    }
-    if lcounts == [1, 1, 1, 2] && countj != 0 {
-        return Kind2::Three;
-    }
-    if lcounts == [1, 1, 1, 2] {
-        return Kind2::One;
-    }
-    if lcounts == [1, 1, 1, 1, 1] && countj != 0 {
-        return Kind2::One;
-    }
-    if lcounts == [1, 1, 1, 1, 1] {
-        return Kind2::High;
-    }
-
-    panic!()
-}
-
-fn n2(card: u8) -> u8 {
-    match card {
-        b'A' => 14,
-        b'K' => 13,
-        b'Q' => 12,
-        b'T' => 10,
-        b'0'..=b'9' => card - b'0',
-        b'J' => 0,
-        _ => panic!(),
-    }
+pub fn part1(input: &Input) -> usize {
+    solve::<false>(input)
 }
 
 pub fn part2(input: &Input) -> usize {
-    input
-        .iter()
-        .map(|&(cards, bid)| (kind2(cards), cards.map(n2), cards, bid))
-        .sorted()
-        .enumerate()
-        .map(|(i, (_, _, _, bid))| (i + 1) * bid)
-        .sum()
+    solve::<true>(input)
 }
