de0521ee32f947a321925b7d853b0a94048b54a5
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-08 10:49:53 +0100

    Optimize day 8

--- src/day8.rs
+++ src/day8.rs
@@ -1,10 +1,16 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = (Vec<LR>, FxHashMap<[u8; 3], ([u8; 3], [u8; 3])>);
 
 pub enum LR {
     L,
     R,
+}
+
+pub struct Input {
+    lr: Vec<LR>,
+    nodes: Vec<[u8; 3]>,
+    left: Vec<usize>,
+    right: Vec<usize>,
 }
 
 pub fn input_generator(input: &str) -> Input {
@@ -12,49 +18,56 @@ pub fn input_generator(input: &str) -> Input {
 
     let lr = lr
         .bytes()
-        .map(|b| if b == b'l' { LR::L } else { LR::R })
+        .map(|b| if b == b'L' { LR::L } else { LR::R })
         .collect();
 
-    let paths = paths
+    let (nodes, ((left, right), node_map)) = paths
         .lines()
-        .map(|line| {
-            let curr = line[..3].as_bytes().try_into().unwrap();
-            let left = line[7..10].as_bytes().try_into().unwrap();
-            let right = line[12..15].as_bytes().try_into().unwrap();
-            (curr, (left, right))
+        .enumerate()
+        .map(|(i, line)| {
+            let node: [u8; 3] = line[..3].as_bytes().try_into().unwrap();
+            let left: [u8; 3] = line[7..10].as_bytes().try_into().unwrap();
+            let right: [u8; 3] = line[12..15].as_bytes().try_into().unwrap();
+            (node, ((left, right), (node, i)))
         })
-        .collect();
-    (lr, paths)
+        .unzip::<_, _, _, ((Vec<_>, Vec<_>), FxHashMap<_, _>)>();
+
+    let left = left.into_iter().map(|node| node_map[&node]).collect();
+    let right = right.into_iter().map(|node| node_map[&node]).collect();
+
+    Input { lr, nodes, left, right }
 }
 
-fn solve(input: &Input, start: [u8; 3], stop: impl Fn(&[u8; 3]) -> bool) -> usize {
-    let (lr, paths) = input;
+fn solve(input: &Input, start: usize, stop: impl Fn(&[u8; 3]) -> bool) -> usize {
     let mut steps = 0;
     let mut curr = start;
-    loop {
-        if stop(&curr) {
-            return steps;
-        }
-        for l in lr {
+    while !stop(&input.nodes[curr]) {
+        for l in &input.lr {
             curr = match l {
-                LR::L => paths[&curr].0,
-                LR::R => paths[&curr].1,
+                LR::L => input.left[curr],
+                LR::R => input.right[curr],
             };
             steps += 1;
         }
     }
+    steps
 }
 
 pub fn part1(input: &Input) -> usize {
-    solve(input, *b"AAA", |curr| curr == b"ZZZ")
+    let aaa = input
+        .nodes
+        .iter()
+        .position(|label| label == b"AAA")
+        .unwrap();
+    solve(input, aaa, |curr| curr == b"ZZZ")
 }
 
 pub fn part2(input: &Input) -> usize {
-    let (_, paths) = input;
-    paths
-        .keys()
-        .copied()
-        .filter(|&curr| curr[2] == b'A')
-        .map(|curr| solve(input, curr, |curr| curr[2] == b'Z'))
+    input
+        .nodes
+        .iter()
+        .enumerate()
+        .filter(|(_, &curr)| curr[2] == b'A')
+        .map(|(i, _)| solve(input, i, |curr| curr[2] == b'Z'))
         .fold(1, num::integer::lcm)
 }
