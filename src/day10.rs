fab2da30558f16f966a321760d22cf5f3dcbf38b
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-10 11:11:45 +0100

    Clean up day 10 solution a bit

--- src/day10.rs
+++ src/day10.rs
@@ -6,90 +6,83 @@ pub fn input_generator(input: &str) -> Input {
     Grid::from_input_chars(input, |char, _, _| char as u8)
 }
 
-pub fn part1(input: &Input) -> usize {
+fn visit_loop(input: &Input, mut f: impl FnMut(usize, usize)) -> u8 {
     let ((x, y), _) = input.iter().find(|&(_, &c)| c == b'S').unwrap();
-    let pos = (x as isize, y as isize);
+    let (mut x, mut y) = (x as isize, y as isize);
 
-    let mut poss = [(pos, (-1, 0)), (pos, (0, 1))];
-    let mut steps = 0;
+    let left = matches!(input.iget((x - 1, y)), Some(b'-' | b'L' | b'F'));
+    let right = matches!(input.iget((x + 1, y)), Some(b'-' | b'7' | b'J'));
+    let top = matches!(input.iget((x, y - 1)), Some(b'|' | b'7' | b'F'));
+    let bottom = matches!(input.iget((x, y + 1)), Some(b'|' | b'L' | b'J'));
 
-    while poss[0].0 != poss[1].0 || steps == 0 {
-        poss = poss.map(|((x, y), (dx, dy))| {
-            let (x, y) = (x + dx, y + dy);
-            let (dx, dy) = match input[(x, y)] {
-                b'-' | b'|' => (dx, dy),
-                b'L' | b'7' => (dy, dx),
-                b'F' | b'J' => (-dy, -dx),
-                _ => unreachable!("{:?} {}", (x, y), input[(x, y)] as char),
-            };
-            ((x, y), (dx, dy))
-        });
-        steps += 1;
+    let (mut dx, mut dy) = match () {
+        _ if left => (-1, 0),
+        _ if right => (1, 0),
+        _ if top => (0, -1),
+        _ if bottom => (0, 1),
+        _ => panic!("Invalid input"),
+    };
+
+    loop {
+        f(x as usize, y as usize);
+        (x, y) = (x + dx, y + dy);
+        (dx, dy) = match input[(x, y)] {
+            b'-' | b'|' => (dx, dy),
+            b'L' | b'7' => (dy, dx),
+            b'F' | b'J' => (-dy, -dx),
+            b'S' => break,
+            _ => panic!("Invalid input"),
+        }
     }
 
-    steps
+    match (left, right, top, bottom) {
+        (true, true, _, _) => b'-',
+        (true, _, true, _) => b'J',
+        (true, _, _, true) => b'7',
+        (_, true, true, _) => b'L',
+        (_, true, _, true) => b'F',
+        (_, _, true, true) => b'|',
+        _ => panic!("Invalid input"),
+    }
+}
+
+pub fn part1(input: &Input) -> usize {
+    let mut steps = 0;
+    visit_loop(input, |_, _| steps += 1);
+    steps / 2
 }
 
 pub fn part2(input: &Input) -> usize {
-    let ((x, y), _) = input.iter().find(|&(_, &c)| c == b'S').unwrap();
-    let pos = (x as isize, y as isize);
+    let mut seen = Grid::with_dimensions(input.w(), input.h()).into_set();
+    let start_symb = visit_loop(input, |x, y| seen[(x, y)] = true);
+    let get_symb = |x, y| match input[(x, y)] {
+        b'S' => start_symb,
+        b => b,
+    };
 
-    let mut poss = [(pos, (-1, 0)), (pos, (0, 1))];
-    let mut first = true;
-
-    let mut seen = HashSet::new();
-    seen.insert(pos);
-
-    while poss[0].0 != poss[1].0 || first {
-        first = false;
-        poss = poss.map(|((x, y), (dx, dy))| {
-            let (x, y) = (x + dx, y + dy);
-            let (dx, dy) = match input[(x, y)] {
-                b'-' | b'|' => (dx, dy),
-                b'L' | b'7' => (dy, dx),
-                b'F' | b'J' => (-dy, -dx),
-                _ => unreachable!("{:?} {}", (x, y), input[(x, y)] as char),
-            };
-            seen.insert((x, y));
-            ((x, y), (dx, dy))
-        });
-    }
-
-    let mut inside = 0;
+    let mut count = 0;
     for y in 0..input.h() {
-        let mut crossed = 0;
-        let mut crossing_dy = 0;
-        for x in 0..input.w() {
-            if seen.contains(&(x as isize, y as isize)) {
-                match input[(x, y)] {
-                    b'|' => {
-                        crossed += 1;
-                        crossing_dy = 0;
+        let mut inside = false;
+        let mut x = 0;
+        while x < input.w() {
+            if seen.contains((x, y)) {
+                let symb = get_symb(x, y);
+                if symb == b'|' {
+                    inside = !inside;
+                } else {
+                    x = (x + 1..).find(|&x| get_symb(x, y) != b'-').unwrap();
+
+                    match (symb, get_symb(x, y)) {
+                        (b'F', b'J') | (b'L', b'7') => inside = !inside,
+                        (b'F', b'7') | (b'L', b'J') => {}
+                        _ => unreachable!(),
                     }
-                    b'-' => {}
-                    b'F' => crossing_dy = 1,
-                    b'L' => crossing_dy = -1,
-                    b'7' | b'S' => {
-                        if crossing_dy == -1 {
-                            crossed += 1;
-                        }
-                        crossing_dy = 0;
-                    }
-                    b'J' => {
-                        if crossing_dy == 1 {
-                            crossed += 1;
-                        }
-                        crossing_dy = 0;
-                    }
-                    _ => unreachable!(),
                 }
-            } else {
-                crossing_dy = 0;
-                if crossed % 2 == 1 {
-                    inside += 1;
-                }
+            } else if inside {
+                count += 1;
             }
+            x += 1;
         }
     }
-    inside
+    count
 }
