a9231d3eec5f2148012749a86e54d42175360b2e
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-18 08:36:47 +0100

    Decent day 18 solution

--- src/day18.rs
+++ src/day18.rs
@@ -17,155 +17,45 @@ pub fn input_generator(input: &str) -> Input {
         .collect()
 }
 
-pub fn part1(input: &Input) -> isize {
-    let (mut minx, mut maxx) = (isize::MAX, isize::MIN);
-    let (mut miny, mut maxy) = (isize::MAX, isize::MIN);
-    let (mut x, mut y) = (0, 0);
-    for &(dir, len, _) in input {
-        let (dx, dy) = match dir {
-            b'L' => (-1, 0),
-            b'R' => (1, 0),
-            b'D' => (0, -1),
-            b'U' => (0, 1),
-            _ => panic!(),
-        };
-        (x, y) = (x + dx * len as isize, y + dy * len as isize);
-        (minx, maxx) = (min(x, minx), max(x, maxx));
-        (miny, maxy) = (min(y, miny), max(y, maxy));
-    }
-
-    let mut cside = b'U';
-    let (mut cx, mut cy) = (0, -miny + 1);
+fn solve(instructions: impl Iterator<Item = (u8, usize)>) -> usize {
     let mut area = 0;
+    let mut perimeter = 0;
+    let mut y = 0;
 
-    let first = (input[input.len() - 1], input[0], input[1]);
-    let last = (input[input.len() - 2], input[input.len() - 1], input[0]);
-
-    let iter = itertools::chain!([first], input.iter().copied().tuple_windows(), [last]);
-
-    for ((dir_p, _, _), (dir, len, _), (dir_n, _, _)) in iter {
-        let malus_in = (cside != dir_p) as usize;
-        let malus_out = (cside == dir_n) as usize;
-        let clen = len + 1 - malus_in - malus_out;
-
-        dbg!(clen);
-        // assert!(cy >= 0);
-
+    for (dir, len) in instructions {
         match dir {
-            b'R' => area += clen as isize * cy,
-            b'L' => area -= clen as isize * cy,
-            _ => {}
-        }
-
-        let (dx, dy) = match dir {
-            b'L' => (-1, 0),
-            b'R' => (1, 0),
-            b'D' => (0, 1),
-            b'U' => (0, -1),
-            _ => panic!(),
-        };
-        (cx, cy) = (cx + dx * clen as isize, cy + dy * clen as isize);
-        cside = match (cside, dir, dir_n) {
-            (b'U', b'R', b'U') | (b'U', b'L', b'D') => b'L',
-            (b'U', b'L', b'U') | (b'U', b'R', b'D') => b'R',
-            (b'D', b'R', b'U') | (b'D', b'L', b'D') => b'R',
-            (b'D', b'L', b'U') | (b'D', b'R', b'D') => b'L',
-
-            (b'L', b'U', b'R') | (b'L', b'D', b'L') => b'U',
-            (b'L', b'D', b'R') | (b'L', b'U', b'L') => b'D',
-            (b'R', b'U', b'R') | (b'R', b'D', b'L') => b'D',
-            (b'R', b'D', b'R') | (b'R', b'U', b'L') => b'U',
+            b'R' => area += len as isize * y,
+            b'L' => area -= len as isize * y,
+            b'U' => y += len as isize,
+            b'D' => y -= len as isize,
             _ => panic!(),
         }
+        perimeter += len;
     }
 
-    area.abs()
+    area.abs() as usize + perimeter / 2 + 1
 }
 
-pub fn part2(input: &Input) -> isize {
-    let input = input
-        .iter()
-        .map(|&(_, _, exa)| {
-            let dir = match exa[5] {
-                b'0' => b'R',
-                b'1' => b'D',
-                b'2' => b'L',
-                b'3' => b'U',
-                _ => panic!(),
-            };
-            let len = exa[..5].iter().fold(0, |acc, &b| {
-                16 * acc
-                    + match b {
-                        b'a'..=b'f' => (b - b'a' + 10) as usize,
-                        b'0'..=b'9' => (b - b'0') as usize,
-                        _ => panic!(),
-                    }
-            });
-            (dir, len)
-        })
-        .collect::<Vec<_>>();
+pub fn part1(input: &Input) -> usize {
+    solve(input.iter().map(|&(dir, len, _)| (dir, len)))
+}
 
-    let (mut minx, mut maxx) = (isize::MAX, isize::MIN);
-    let (mut miny, mut maxy) = (isize::MAX, isize::MIN);
-    let (mut x, mut y) = (0, 0);
-    for &(dir, len) in &input {
-        let (dx, dy) = match dir {
-            b'L' => (-1, 0),
-            b'R' => (1, 0),
-            b'D' => (0, -1),
-            b'U' => (0, 1),
-            _ => panic!(),
-        };
-        (x, y) = (x + dx * len as isize, y + dy * len as isize);
-        (minx, maxx) = (min(x, minx), max(x, maxx));
-        (miny, maxy) = (min(y, miny), max(y, maxy));
-    }
-
-    // TODO: Depends on input + inside/outside
-    let mut cside = b'D';
-    let (mut cx, mut cy) = (0, 0);
-    let mut area = 0;
-
-    let first = (input[input.len() - 1], input[0], input[1]);
-    let last = (input[input.len() - 2], input[input.len() - 1], input[0]);
-
-    let iter = itertools::chain!([first], input.iter().copied().tuple_windows(), [last]);
-
-    for ((dir_p, _), (dir, len), (dir_n, _)) in iter {
-        let malus_in = (cside != dir_p) as usize;
-        let malus_out = (cside == dir_n) as usize;
-        let clen = len + 1 - malus_in - malus_out;
-
-        dbg!(clen);
-        // assert!(cy >= 0);
-
-        match dir {
-            b'R' => area += clen as isize * cy,
-            b'L' => area -= clen as isize * cy,
-            _ => {}
-        }
-
-        let (dx, dy) = match dir {
-            b'L' => (-1, 0),
-            b'R' => (1, 0),
-            b'D' => (0, 1),
-            b'U' => (0, -1),
-            _ => panic!(),
-        };
-        (cx, cy) = (cx + dx * clen as isize, cy + dy * clen as isize);
-        cside = match (cside, dir, dir_n) {
-            (b'U', b'R', b'U') | (b'U', b'L', b'D') => b'L',
-            (b'U', b'L', b'U') | (b'U', b'R', b'D') => b'R',
-            (b'D', b'R', b'U') | (b'D', b'L', b'D') => b'R',
-            (b'D', b'L', b'U') | (b'D', b'R', b'D') => b'L',
-
-            (b'L', b'U', b'R') | (b'L', b'D', b'L') => b'U',
-            (b'L', b'D', b'R') | (b'L', b'U', b'L') => b'D',
-            (b'R', b'U', b'R') | (b'R', b'D', b'L') => b'D',
-            (b'R', b'D', b'R') | (b'R', b'U', b'L') => b'U',
-            _ => panic!(),
-        }
-    }
-
-    area.abs()
+pub fn part2(input: &Input) -> usize {
+    let hex_to_dir = |b| match b {
+        b'0' => b'R',
+        b'1' => b'D',
+        b'2' => b'L',
+        b'3' => b'U',
+        _ => panic!(),
+    };
+    let hex_to_num = |b| match b {
+        b'a'..=b'f' => (b - b'a' + 10) as usize,
+        b'0'..=b'9' => (b - b'0') as usize,
+        _ => panic!(),
+    };
+    solve(input.iter().map(|&(_, _, exa)| {
+        let dir = hex_to_dir(exa[5]);
+        let len = exa[..5].iter().fold(0, |acc, &b| 16 * acc + hex_to_num(b));
+        (dir, len)
+    }))
 }
