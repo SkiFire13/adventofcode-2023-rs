2dad688e9a9685919b68c090aa4f0bb14e34a9a8
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-20 10:03:31 +0100

    Simplify day 20 solution

--- src/day20.rs
+++ src/day20.rs
@@ -1,207 +1,125 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input<'a> = (
-    Vec<&'a str>,
-    FxHashMap<&'a str, Vec<&'a str>>,
-    FxHashMap<&'a str, Vec<&'a str>>,
-);
+type Input<'a> = FxHashMap<&'a str, (Vec<&'a str>, u8, Vec<&'a str>)>;
 
 pub fn input_generator(input: &str) -> Input {
-    let mut broadcaster = Vec::new();
-    let mut inv = FxHashMap::default();
-    let mut conj = FxHashMap::default();
+    let mut map = Input::default();
     for line in input.lines() {
-        let (key, outputs) = line.split_once(" -> ").unwrap();
-        let outputs = outputs.split(", ").collect();
-        if key == "broadcaster" {
-            broadcaster = outputs;
-        } else if key.starts_with("%") {
-            inv.insert(&key[1..], outputs);
-        } else {
-            assert!(key.starts_with("&"));
-            conj.insert(&key[1..], outputs);
+        let (key, post) = line.split_once(" -> ").unwrap();
+        let post = post.split(", ").collect();
+
+        let kind = key.as_bytes()[0];
+        let key = &key[1..];
+
+        for &output in &post {
+            map.entry(output).or_default().0.push(key);
+        }
+
+        let entry = map.entry(key).or_default();
+        entry.1 = kind;
+        entry.2 = post;
+    }
+    map
+}
+
+fn solve(input: &Input, mut cb: impl FnMut(usize, &str, bool, &str) -> ControlFlow<()>) {
+    let mut inv_states = input
+        .iter()
+        .filter(|&(_, &(_, kind, _))| kind == b'%')
+        .map(|(&key, _)| (key, false))
+        .collect::<FxHashMap<_, _>>();
+    let mut conj_states = input
+        .iter()
+        .filter(|&(_, &(_, kind, _))| kind == b'&')
+        .map(|(&key, (pre, _, _))| {
+            let prevs_state = pre
+                .iter()
+                .map(|&prev| (prev, false))
+                .collect::<FxHashMap<_, _>>();
+            (key, (0, prevs_state))
+        })
+        .collect::<FxHashMap<_, _>>();
+
+    for i in 1.. {
+        let mut queue = VecDeque::from([("button", false, "roadcaster")]);
+        while let Some((prev, is_high, curr)) = queue.pop_front() {
+            if cb(i, prev, is_high, curr).is_break() {
+                return;
+            }
+
+            let (_, kind, post) = &input[curr];
+            match kind {
+                b'b' => queue.extend(post.iter().map(|&next| (curr, is_high, next))),
+                b'%' => {
+                    if !is_high {
+                        let out_is_high = inv_states.entry(curr).or_default();
+                        *out_is_high = !*out_is_high;
+                        queue.extend(post.iter().map(|&next| (curr, *out_is_high, next)));
+                    }
+                }
+                b'&' => {
+                    let (count, prevs_state) = conj_states.get_mut(curr).unwrap();
+                    let prev_is_high = prevs_state.get_mut(prev).unwrap();
+
+                    match (is_high, *prev_is_high) {
+                        (true, false) => *count += 1,
+                        (false, true) => *count -= 1,
+                        _ => {}
+                    }
+
+                    *prev_is_high = is_high;
+                    let out_high = *count != prevs_state.len();
+                    queue.extend(post.iter().map(|&next| (curr, out_high, next)));
+                }
+                0 => {}
+                _ => unreachable!(),
+            }
         }
     }
-    (broadcaster, inv, conj)
 }
 
 pub fn part1(input: &Input) -> usize {
-    let (broadcaster, inv, conj) = input;
-
-    let mut inv_states = inv
-        .keys()
-        .map(|&key| (key, false))
-        .collect::<FxHashMap<_, _>>();
-    let mut conj_states = conj
-        .keys()
-        .map(|&key| (key, FxHashMap::default()))
-        .collect::<FxHashMap<_, _>>();
-    let mut conj_states_count = conj
-        .keys()
-        .map(|&key| (key, 0))
-        .collect::<FxHashMap<_, _>>();
-
-    for &output in broadcaster {
-        if let Some(conj_state) = conj_states.get_mut(output) {
-            conj_state.insert("broadcaster", false);
-        }
-    }
-    for (key, outputs) in inv {
-        for output in outputs {
-            if let Some(conj_state) = conj_states.get_mut(output) {
-                conj_state.insert(key, false);
-            }
-        }
-    }
-    for (key, outputs) in conj {
-        for output in outputs {
-            if let Some(conj_state) = conj_states.get_mut(output) {
-                conj_state.insert(key, false);
-            }
-        }
-    }
-
-    // println!("{:?}", inv_states);
-    // println!("{:?}", conj_states);
-    // println!("{:?}", conj_states_count);
     let (mut lows, mut highs) = (0, 0);
-    for _ in 0..1000 {
-        let mut queue = VecDeque::from([("button", "broadcaster", false)]);
-        while let Some((prev, curr, level)) = queue.pop_front() {
-            // println!("{prev} -{level}-> {curr}");
-            if level {
-                highs += 1;
-            } else {
-                lows += 1;
-            }
-            if curr == "broadcaster" {
-                queue.extend(broadcaster.iter().map(|&next| (curr, next, level)));
-            } else if inv.contains_key(curr) {
-                if !level {
-                    let state = inv_states.entry(curr).or_default();
-                    *state = !*state;
-                    queue.extend(inv[curr].iter().map(|&next| (curr, next, *state)));
-                }
-            } else if conj.contains_key(curr) {
-                assert!(conj_states.contains_key(curr));
-                assert!(conj_states_count.contains_key(curr));
-                let entry = conj_states.get_mut(curr).unwrap().get_mut(prev).unwrap();
-                let before = *entry;
-                *entry = level;
-                if level {
-                    if !before {
-                        *conj_states_count.get_mut(curr).unwrap() += 1;
-                    }
-                    let signal = conj_states_count[curr] != conj_states[curr].len();
-                    queue.extend(conj[curr].iter().map(|&next| (curr, next, signal)));
-                } else {
-                    if before {
-                        *conj_states_count.get_mut(curr).unwrap() -= 1;
-                    }
-                    queue.extend(conj[curr].iter().map(|&next| (curr, next, true)));
-                }
-            }
+
+    solve(input, |i, _, is_high, _| {
+        if i == 1001 {
+            return ControlFlow::Break(());
         }
-        // println!("{:?}", inv_states);
-        // println!("{:?}", conj_states);
-        // println!("{:?}", conj_states_count);
-    }
+
+        match is_high {
+            true => highs += 1,
+            false => lows += 1,
+        }
+
+        ControlFlow::Continue(())
+    });
 
     lows * highs
 }
 
 pub fn part2(input: &Input) -> usize {
-    let (broadcaster, inv, conj) = input;
-
-    let mut inv_states = inv
-        .keys()
-        .map(|&key| (key, false))
-        .collect::<FxHashMap<_, _>>();
-    let mut conj_states = conj
-        .keys()
-        .map(|&key| (key, FxHashMap::default()))
-        .collect::<FxHashMap<_, _>>();
-    let mut conj_states_count = conj
-        .keys()
-        .map(|&key| (key, 0))
-        .collect::<FxHashMap<_, _>>();
-
-    for &output in broadcaster {
-        if let Some(conj_state) = conj_states.get_mut(output) {
-            conj_state.insert("broadcaster", false);
-        }
-    }
-    for (key, outputs) in inv {
-        for output in outputs {
-            if let Some(conj_state) = conj_states.get_mut(output) {
-                conj_state.insert(key, false);
-            }
-        }
-    }
-    for (key, outputs) in conj {
-        for output in outputs {
-            if let Some(conj_state) = conj_states.get_mut(output) {
-                conj_state.insert(key, false);
-            }
-        }
-    }
-
-    let (&rx_prev, _) = conj
+    let (rx_pre, _, _) = &input["rx"];
+    assert_eq!(rx_pre.len(), 1);
+    let (rx_pre_pre, rx_pre_kind, _) = &input[&rx_pre[0]];
+    assert_eq!(rx_pre_kind, &b'&');
+    let mut rx_pre_pre = rx_pre_pre
         .iter()
-        .find(|&(_, outputs)| outputs.contains(&"rx"))
-        .unwrap();
-    let mut rx_prev_prevs = conj
-        .iter()
-        .filter(|&(_, outputs)| outputs.contains(&rx_prev))
-        .map(|(&key, _)| (key, 0))
+        .map(|&prev| (prev, 0))
         .collect::<FxHashMap<_, _>>();
-    let mut rx_prev_prevs_seen = 0;
+    let mut rx_pre_pre_seen = 0;
 
-    for i in 1.. {
-        let mut queue = VecDeque::from([("button", "broadcaster", false)]);
-        while let Some((prev, curr, level)) = queue.pop_front() {
-            if level && curr == rx_prev {
-                if rx_prev_prevs[prev] == 0 {
-                    rx_prev_prevs.insert(prev, i);
-                    rx_prev_prevs_seen += 1;
-                    if rx_prev_prevs_seen == rx_prev_prevs.len() {
-                        return rx_prev_prevs.values().product();
-                    }
-                }
-            }
-
-            if !level && curr == "rx" {
-                return i;
-            }
-            if curr == "broadcaster" {
-                queue.extend(broadcaster.iter().map(|&next| (curr, next, level)));
-            } else if inv.contains_key(curr) {
-                if !level {
-                    let state = inv_states.entry(curr).or_default();
-                    *state = !*state;
-                    queue.extend(inv[curr].iter().map(|&next| (curr, next, *state)));
-                }
-            } else if conj.contains_key(curr) {
-                assert!(conj_states.contains_key(curr));
-                assert!(conj_states_count.contains_key(curr));
-                let entry = conj_states.get_mut(curr).unwrap().get_mut(prev).unwrap();
-                let before = *entry;
-                *entry = level;
-                if level {
-                    if !before {
-                        *conj_states_count.get_mut(curr).unwrap() += 1;
-                    }
-                    let signal = conj_states_count[curr] != conj_states[curr].len();
-                    queue.extend(conj[curr].iter().map(|&next| (curr, next, signal)));
-                } else {
-                    if before {
-                        *conj_states_count.get_mut(curr).unwrap() -= 1;
-                    }
-                    queue.extend(conj[curr].iter().map(|&next| (curr, next, true)));
+    solve(input, |i, prev, is_high, curr| {
+        if is_high && curr == rx_pre[0] {
+            if let Some(prev_iter @ 0) = rx_pre_pre.get_mut(prev) {
+                *prev_iter = i;
+                rx_pre_pre_seen += 1;
+                if rx_pre_pre_seen == rx_pre_pre.len() {
+                    return ControlFlow::Break(());
                 }
             }
         }
-    }
+        ControlFlow::Continue(())
+    });
 
-    unreachable!()
+    return rx_pre_pre.values().product();
 }
