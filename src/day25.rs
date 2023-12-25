1fdfbc22d3458b6170b1072f29973cc3458f4091
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-25 13:04:20 +0100

    Actually solve day 25

--- src/day25.rs
+++ src/day25.rs
@@ -1,25 +1,11 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = Vec<Vec<usize>>;
+type Input = (Vec<Vec<(usize, usize)>>, usize);
 
 pub fn input_generator(input: &str) -> Input {
     let mut node_to_id = FxHashMap::default();
     let mut edges = Vec::new();
-
-    // Nodes connected to the critical edges
-    // Carefully chosen.
-    node_to_id.insert("vzb", 0);
-    node_to_id.insert("krx", 1);
-    node_to_id.insert("tqn", 2);
-    edges.push(Vec::new());
-    edges.push(Vec::new());
-    edges.push(Vec::new());
-
-    // One node per side
-    node_to_id.insert("sbk", 3);
-    node_to_id.insert("xmh", 4);
-    edges.push(Vec::new());
-    edges.push(Vec::new());
+    let mut next_edge_id = 0;
 
     for line in input.lines() {
         let (key, rest) = line.split_once(": ").unwrap();
@@ -29,42 +15,102 @@ pub fn input_generator(input: &str) -> Input {
             edges.len() - 1
         });
 
-        // Don't add edges connected to the critical nodes.
-        if key_id < 3 {
-            continue;
-        }
-
         for other in rest.split(' ') {
             let other_id = *node_to_id.entry(other).or_insert_with(|| {
                 edges.push(Vec::new());
                 edges.len() - 1
             });
 
-            // Don't add edges connected to the critical nodes.
-            if other_id < 3 {
-                continue;
-            }
-
-            edges[key_id].push(other_id);
-            edges[other_id].push(key_id);
+            edges[key_id].push((other_id, next_edge_id));
+            edges[other_id].push((key_id, next_edge_id));
+            next_edge_id += 1;
         }
     }
-    edges
-}
-
-fn dfs(input: &Input, start: usize) -> usize {
-    let mut seen = FxHashSet::default();
-    let mut queue = vec![start];
-
-    while let Some(curr) = queue.pop() {
-        if seen.insert(curr) {
-            queue.extend(&input[curr]);
-        }
-    }
-
-    seen.len()
+    (edges, next_edge_id)
 }
 
 pub fn part1(input: &Input) -> usize {
-    (dfs(input, 3) + 1) * (dfs(input, 4) + 2)
+    let &(ref node_to_edges, edge_count) = input;
+
+    let mut set1 = bitbox![0; node_to_edges.len()];
+    let mut min_cut = bitbox![0; edge_count];
+    let mut min_cut_len = 0;
+    let mut to_visit = Vec::<(usize, usize)>::new();
+    let mut visited = bitbox![0; node_to_edges.len()];
+
+    set1.set(0, true);
+    visited.set(0, true);
+    to_visit.extend(&node_to_edges[0]);
+
+    let mut edges_seen = bitbox![0; edge_count];
+    let mut prev = vec![(usize::MAX, usize::MAX); node_to_edges.len()];
+    let mut queue = VecDeque::new();
+
+    'outer: while let Some((curr, edge)) = to_visit.pop() {
+        if visited.replace(curr, true) {
+            continue;
+        }
+
+        edges_seen.fill(false);
+
+        'inner: for _ in 0..4 {
+            prev.fill((usize::MAX, usize::MAX));
+            prev[curr] = (usize::MAX - 1, usize::MAX - 1);
+            queue.clear();
+            for &(node, edge) in &node_to_edges[curr] {
+                if !edges_seen[edge] {
+                    prev[node] = (curr, edge);
+                    queue.push_back((node, edge));
+                }
+            }
+
+            while let Some((mut curr, mut edge)) = queue.pop_front() {
+                if set1[curr] {
+                    while curr != usize::MAX - 1 {
+                        edges_seen.set(edge, true);
+                        (curr, edge) = prev[curr];
+                    }
+                    continue 'inner;
+                }
+                for &(next, next_edge) in &node_to_edges[curr] {
+                    if !edges_seen[next_edge] && prev[next] == (usize::MAX, usize::MAX) {
+                        prev[next] = (curr, edge);
+                        queue.push_back((next, next_edge));
+                    }
+                }
+            }
+
+            min_cut.set(edge, true);
+            min_cut_len += 1;
+            if min_cut_len == 3 {
+                let mut prod = 1;
+                for start in [0, curr] {
+                    let mut seen = FxHashSet::default();
+                    let mut queue = vec![start];
+
+                    while let Some(curr) = queue.pop() {
+                        if seen.insert(curr) {
+                            for &(node, edge) in &node_to_edges[curr] {
+                                if !min_cut[edge] {
+                                    queue.push(node);
+                                }
+                            }
+                        }
+                    }
+
+                    prod *= seen.len();
+                }
+                return prod;
+            }
+
+            continue 'outer;
+        }
+
+        set1.set(curr, true);
+        for &(node, edge) in &node_to_edges[curr] {
+            to_visit.push((node, edge));
+        }
+    }
+
+    unreachable!()
 }
