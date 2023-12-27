76f4ddf631335fea963abb0cbe60093ddbea4120
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-12-27 11:33:11 +0100

    Improve day 12 performance a bit

--- src/day12.rs
+++ src/day12.rs
@@ -14,49 +14,57 @@ pub fn input_generator(input: &str) -> Input {
         .collect()
 }
 
-fn count(pat: &[u8], nums: &[usize]) -> usize {
-    let mut dp = vec![0; (pat.len() + 1) * (nums.len() + 1)];
+fn count(pat: &[u8], nums: &[usize], dp: &mut Vec<usize>) -> usize {
+    dp.resize((pat.len() + 1) * 2, 0);
+    let (mut curr_dp, mut prev_dp) = dp.split_at_mut(pat.len() + 1);
 
-    let offset = (pat.len() + 1) * nums.len();
-    dp[offset + pat.len()] = 1;
+    prev_dp.fill(0);
+    prev_dp[pat.len()] = 1;
     for j in (0..pat.len()).rev().take_while(|&j| pat[j] != b'#') {
-        dp[offset + j] = 1;
+        prev_dp[j] = 1;
     }
 
-    for i in (0..nums.len()).rev() {
+    for &n in nums.iter().rev() {
         let mut streak = 0;
-        for j in (0..pat.len()).rev() {
-            streak += 1;
-            if pat[j] == b'.' {
-                streak = 0;
-            }
+        curr_dp[pat.len()] = 0;
+        for (j, &p) in pat.iter().enumerate().rev() {
+            streak = if p == b'.' { 0 } else { streak + 1 };
+            curr_dp[j] = if p == b'#' { 0 } else { curr_dp[j + 1] };
 
-            if pat[j] != b'#' {
-                dp[(pat.len() + 1) * i + j] = dp[(pat.len() + 1) * i + j + 1];
-            }
-
-            let prev = j.checked_sub(1).map(|j| pat[j]).unwrap_or(b'.');
-            let next = pat.get(j + nums[i]).copied().unwrap_or(b'.');
-            if streak >= nums[i] && prev != b'#' && next != b'#' {
-                let next_idx = j + nums[i] + (j + nums[i] < pat.len()) as usize;
-                dp[(pat.len() + 1) * i + j] += dp[(pat.len() + 1) * (i + 1) + next_idx];
+            let prev_pat = pat.get(j.wrapping_sub(1));
+            let next = pat.get(j + n);
+            if streak >= n && prev_pat != Some(&b'#') && next != Some(&b'#') {
+                curr_dp[j] += prev_dp[j + n + (j + n < pat.len()) as usize];
             }
         }
+        swap(&mut curr_dp, &mut prev_dp);
     }
 
-    dp[0]
+    prev_dp[0]
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.iter().map(|(pat, nums)| count(pat, nums)).sum()
+    let mut dp = Vec::new();
+    input
+        .iter()
+        .map(|(pat, nums)| count(pat, nums, &mut dp))
+        .sum()
 }
 
 pub fn part2(input: &Input) -> usize {
+    let mut dp = Vec::new();
+    let mut new_pat = Vec::new();
+    let mut new_nums = Vec::new();
     input
         .iter()
-        .map(|(pat, nums)| {
-            let mut new_pat = pat.to_vec();
-            let mut new_nums = nums.to_vec();
+        .map(|&(pat, ref nums)| {
+            new_pat.clear();
+            new_pat.reserve(pat.len() * 5 + 4);
+            new_pat.extend_from_slice(pat);
+
+            new_nums.clear();
+            new_nums.reserve(nums.len() * 5);
+            new_nums.extend_from_slice(nums);
 
             for _ in 1..5 {
                 new_pat.push(b'?');
@@ -64,7 +72,7 @@ pub fn part2(input: &Input) -> usize {
                 new_nums.extend_from_slice(nums);
             }
 
-            count(&new_pat, &new_nums)
+            count(&new_pat, &new_nums, &mut dp)
         })
         .sum()
 }
